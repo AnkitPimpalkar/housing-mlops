2f438c98881371979b04527d1ff5935af3d15aae
unknown <ankitpimpalkar707@gmail.com>
2025-05-10 13:49:58 +0530

    Update CI workflow and setup.py for src-based project structure

--- setup.py
+++ setup.py
@@ -3,14 +3,15 @@ from setuptools import setup, find_packages
 setup(
     name='housing_price_prediction',
     version='0.1',
-    packages=find_packages(),
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     install_requires=[
         'pandas',
         'scikit-learn',
         'matplotlib',
         'seaborn',
         'mlflow',
-        "pytest"
+        'pytest'
     ],
     author='Ankit Pimpalkar',
     description='A modular housing price prediction project with MLOps practices.',
